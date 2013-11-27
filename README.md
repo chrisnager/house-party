9e2b7647414d292e4648fc7e73a7e3fd24680896
David Mackey <tdmackey@booleanhaiku.com>
2013-11-26 22:26:02 -0800

    Add basic Facebook Auth

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # House Party
 
+## Running
+
+First, install nessecary gems via bundler.
+`$ bundle install`
+
+Second, it is nessecary to provide the proper Facbeook credentials.
+The APP SECRET and APP ID can be obtained from the facebook developer application page: https://developers.facebook.com/apps/1463273950565798/summary
+Dev app credentials can be obtained from: https://developers.facebook.com/apps/419480054847402/summary
+
+To provide these running locally set them in the .env file. 
+To provide them to heroku see https://devcenter.heroku.com/articles/config-vars.
+The committted .env file contains the credentials for the -DEV app. Heroku is set to use production credentials.
+
+Run app locally via foreman.
+`$ foreman start`
+
+Push to Heroku to deploy.
+`$ git push heroku master`
+
+To push a branch other than master you can:
+`$ git push heroku yourbranch:master`
+
+## Notes
+
+This app uses [Koala](https://github.com/arsduo/koala) to talk to the Facebook API.
+
 Crafted by @chrisnager and @tdmackey.
--- web.rb
+++ web.rb
@@ -1,24 +1,61 @@
-require 'sinatra'
-require 'haml'
+#let Bundler handle all requires
+Bundler.require(:default)
+
+#These variables are being read in from .env file or heroku config.
+unless ENV["FACEBOOK_APP_ID"] && ENV["FACEBOOK_APP_SECRET"]
+	  abort("missing env vars: please set FACEBOOK_APP_ID and FACEBOOK_SECRET with your app credentials")
+end
+
+use Rack::Session::Cookie, secret: 'SUPER_SECRET_SWAGTASTIC_HOUSE_PARTY_APP_'
 
 get '/' do
-	haml :main
+	if session['access_token']
+		haml :main
+	else
+		'<a href="/login">Login</a>'
+	end
 end
-
-
 
 get '/find-a-party' do
-	haml :find_a_party
+	if session['access_token']
+		haml :find_a_party
+	else
+		'<a href="/login">Login</a>'
+	end
 end
-
-
 
 get '/host-a-party' do
-	haml :host_a_party
+	if session['access_token']
+		haml :host_a_party
+	else
+		'<a href="/login">Login</a>'
+	end
 end
 
-
-
 get '/party-details' do
-	haml :party_details
+	if session['access_token']
+		haml :party_details
+	else
+		'<a href="/login">Login</a>'
+	end
+end
+  
+get '/login' do
+	# generate a new oauth object
+ 	session['oauth'] = Koala::Facebook::OAuth.new(ENV["FACEBOOK_APP_ID"], ENV["FACEBOOK_APP_SECRET"], "#{request.base_url}/callback")
+	# redirect to facebook
+ 	redirect session['oauth'].url_for_oauth_code()
+ end
+  
+get '/logout' do
+	session['oauth'] = nil
+	session['access_token'] = nil
+	redirect '/'
+end
+
+#method to handle the redirect from facebook back to us
+get '/callback' do
+	#get the access token from facebook
+	session['access_token'] = session['oauth'].get_access_token(params[:code])
+	redirect '/'
 end
