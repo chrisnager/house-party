62a23de6487b0163023c9cee597da5a53014cd5f
Chris Nager <cnager@gmail.com>
2013-11-26 21:51:13 -0500

    Build layout view and separate page views

--- web.rb
+++ web.rb
@@ -4,3 +4,21 @@ require 'haml'
 get '/' do
 	haml :main
 end
+
+
+
+get '/find-a-party' do
+	haml :find_a_party
+end
+
+
+
+get '/host-a-party' do
+	haml :host_a_party
+end
+
+
+
+get '/party-details' do
+	haml :party_details
+end
