12a5aeadd4a73a1968726670a5f265f4e7d52b9c
Chris Nager <cnager@gmail.com>
2013-12-01 20:26:41 -0500

    Add selected state to friend list

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
+$( function() {
 
+    // Select friends in facebook friend list
+    $( '.friend-list__item' ).on( 'click', 'a', function( e ) {
+        $( this ).parent().toggleClass( 'is-selected' );
+        e.preventDefault;
+    });
+
+});
